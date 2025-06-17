e55b391b1b73294ff5c14bf76e5e222e1503be1e
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-06-17 17:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+    const textInput = document.getElementById('evaluatedText');
+    const letterCount = document.getElementById('letterCount');
+
+    textInput.addEventListener('input', () => {
+      letterCount.textContent = textInput.value.length;
+    });
+ 
